68cc83a3f26156552b4049f6e6069880bc15f810
Max Walder <max.walder@protonmail.com>
2024-04-17 15:57:49 +0200

    metrics

--- compute_all_metrics.py
+++ compute_all_metrics.py
@@ -3,12 +3,12 @@ import os.path
 from plots import plot_main
 
 experiments_to_evaluate = [
+    '10k_bpr_rank_based',
+    '10k_bpr_us_centric',
     '100k_sample1_bpr_rank_based',
     '100k_sample1_neumf_rank_based',
     '100k_sample1_itemknn_rank_based',
     '100k_sample1_multivae_rank_based',
-    '10k_bpr_rank_based',
-    # '10k_bpr_us_centric'
     # add more experiments here
 ]
 
